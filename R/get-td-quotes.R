66b7bd6a8bd6b15bebf3ca4a140a2341744388a7
munair <munair@gmail.com>
2023-09-12 19:50:48 -0400

    update code to use R package for retrieving a fresh access token

--- R/get-td-quotes.R
+++ R/get-td-quotes.R
@@ -4,7 +4,7 @@
 #                                     get quotes information using the asset's symbol
 #
 # created by:                         munair
-# updated on:                         wednesday, august 23rd 2023
+# updated on:                         tuesday, september 12th, 2023
 #
 # tda rest api endpoint:              https://api.tdameritrade.com/v1/marketdata/quotes
 # quotes generation link:             https://developer.tdameritrade.com/quotes/apis/get/marketdata/quotes
@@ -12,30 +12,42 @@
 # to do:                              support multiple quotes
 #
 # remove the comment delimiter in the section below to install required libraries:
-# install.packages("httr")
-require("httr")
-require("jsonlite")
+# devtools::install( "../R-packages/refreshtdaccesstoken" )
+# install.packages( "httr" )
+
+# load libraries
+require( "refreshtdaccesstoken" )
+require( "jsonlite" )
+require( "httr" )
 
 # define input variables
-assetsymbol = "OXY" # symbol of the financial instrument to get quotes on
+assetsymbol <- "OXY" # symbol of the financial instrument to get quotes on
+
+# refresh td ameritrade access token
+accesstoken <- refreshtdaccesstoken::refreshtdaccesstoken(
+  callbackurl = "https://mitzvah.capital", # define callback url
+  consumerkey = paste0( TD$APIKEY, "@AMER.OAUTHAP"), # define consumer key
+  storedtoken = readRDS( file = "~/Documents/tdameritrade-refresh-token.rds" ) # retrieve stored refresh token
+)
 
 # make http rest api request
-bearertoken = paste0( "Bearer ", ACCESSTOKEN )
-endpointurl = paste0( "https://api.tdameritrade.com/v1/marketdata/quotes?symbol=", assetsymbol )
-httprequest = httr::GET(  endpointurl, httr::add_headers( 'Authorization' = bearertoken ) )
-httpcontent = httr::content( httprequest, "text", encoding = "UTF-8" )
-listcontent = fromJSON( httpcontent )
+bearertoken <- paste0( "Bearer ", accesstoken )
+endpointurl <- paste0( "https://api.tdameritrade.com/v1/marketdata/quotes?symbol=", assetsymbol )
+httprequest <- httr::GET(  endpointurl, httr::add_headers( 'Authorization' = bearertoken ) )
+httpcontent <- httr::content( httprequest, "parsed", encoding = "UTF-8" )
+
+# bind list data into one column
+contentlist <- do.call( cbind, httpcontent )
 
 # format dates
-listcontent[[assetsymbol]]$quoteTimeInLong <- as.POSIXct( listcontent[[assetsymbol]]$quoteTimeInLong/1000, origin = "1970-01-01" ) 
-listcontent[[assetsymbol]]$tradeTimeInLong <- as.POSIXct( listcontent[[assetsymbol]]$tradeTimeInLong/1000, origin = "1970-01-01" ) 
-listcontent[[assetsymbol]]$regularMarketTradeTimeInLong <- as.POSIXct( listcontent[[assetsymbol]]$regularMarketTradeTimeInLong/1000, origin = "1970-01-01" ) 
+contentlist[ "quoteTimeInLong", ][[ 1 ]] <- as.POSIXct( contentlist[ "quoteTimeInLong", ][[ 1 ]] / 1000, origin = "1970-01-01" ) 
+contentlist[ "tradeTimeInLong", ][[ 1 ]] <- as.POSIXct( contentlist[ "tradeTimeInLong", ][[ 1 ]] / 1000, origin = "1970-01-01" ) 
+if( "lastTradingDay" %in% rownames( contentlist ) ) { contentlist[ "lastTradingDay", ][[ 1 ]] <- as.POSIXct( contentlist[ "lastTradingDay", ][[ 1 ]] / 1000, origin = "1970-01-01" ) }
+if( "regularMarketTradeTimeInLong" %in% rownames( contentlist ) ) { contentlist[ "regularMarketTradeTimeInLong", ][[ 1 ]] <- as.POSIXct( contentlist[ "regularMarketTradeTimeInLong", ][[ 1 ]] / 1000, origin = "1970-01-01" ) }
+
 
 # format large numbers
-listcontent[[assetsymbol]][["totalVolume"]] <- prettyNum( listcontent[[assetsymbol]][["totalVolume"]], big.mark = "," )
+contentlist[ "totalVolume", ][[ 1 ]] <- prettyNum( contentlist[ "totalVolume", ][[ 1 ]], big.mark = "," )
 
-# view transposed dataframe of response
-dataframery <- data.frame( listcontent )
-dataframery <- t( dataframery )
-colnames( dataframery ) <- assetsymbol
-View( dataframery )
+# view response
+View( contentlist, paste0( assetsymbol, " Quotation" ) ) # to verify successful formatting: View( contentlist[ "regularMarketTradeTimeInLong", ][[ 1 ]] )
